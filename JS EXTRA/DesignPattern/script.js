cf5ce66ad7ef68022794f7f5550e479be0eaeff9
saksham <sakshamsinghrajput12345@gmail.com>
2026-08-24 23:49:27 +0530

    Update JS EXTRA

---
+++ JS EXTRA/DesignPattern/script.js
@@ -0,0 +1,14 @@
+// Design Pattern --> 
+
+// Module Pattern ek design Pattern hai jisme hum aapna code ek self executing function (IIFE ) 
+// ke under likhte hain , taki variables aur function private rhenn.
+
+// Iske aunder hmm sirf wahi cheezien return karte hain jo bahar use karni hain
+
+// Is pattern ka main fayda hai data hiding (encapsulation ) aur clean 
+// structure, taaki code source , reusable and manageble ban sakke.
+
+ let f1 = (function(){
+
+ })();
+ 
